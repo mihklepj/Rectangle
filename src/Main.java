b1a2319422dd9903c3f778c05546710feae19b56
mihklepj <justinmihklep@msn.com>
2023-05-17 22:24:26 +0300

    Final

--- src/Main.java
+++ src/Main.java
@@ -8,21 +8,30 @@ class Rectangle {
     {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
+
+        // Random numbri loomine
         int rand;
         do {
             rand = random.nextInt(11);
         } while (rand == 0);
 
+        // Kasutajalt küsitakse ristküliku pikkust
         System.out.print("Enter the length of Rectangle: ");
         double length = scanner.nextDouble();
 
+        // Ristküliku laiuseks määratakse random number
         double width = rand;
 
+        // Diagonaali arvutus. (d = √( l2 + w2))
         double diagonal = sqrt(pow(length,2)+pow(width,2));
 
-        // Area = length*width;
+        // Pindala arvutus.
         double area = length*width;
+
+        // Ümbermõõdu arvutus.
         double perimeter = 2*(length+width);
+
+        // Vastuse väljastus.
         System.out.printf("""
                 Width of the rectangle is %.0f,
                 diagonal of the rectangle is %.1f,
